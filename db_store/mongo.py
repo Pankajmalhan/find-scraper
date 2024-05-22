3180f7ae79a622ed76b550b419ddba7c19397346
pankajmalhan <pankajmalhan30@gmail.com>
2024-05-22 19:05:16 +0530

    first commit

---
+++ db_store/mongo.py
@@ -0,0 +1,72 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file (if it exists)
+dotenv_path = os.path.join(os.getcwd(), ".env")  # Construct path relative to script
+if os.path.isfile(dotenv_path):
+    load_dotenv(dotenv_path)
+    
+
+MONGODB_ATLAS_CLUSTER_URI = os.getenv('MONGODB_ATLAS_CLUSTER_URI')
+DB_NAME = "accesfind"
+COLLECTION_NAME = "websites"
+ATLAS_VECTOR_SEARCH_INDEX_NAME = "vector_index"
+
+def get_website_collection():
+       # initialize MongoDB python client
+       client = MongoClient(MONGODB_ATLAS_CLUSTER_URI)
+       MONGODB_COLLECTION = client[DB_NAME][COLLECTION_NAME]
+       return MONGODB_COLLECTION
+
+def get_websites(skip=0, limit=20):
+       collection =  get_website_collection();
+       websites = collection.find({}).skip(skip).limit(limit)
+       return websites
+
+def is_website_exists(domain):
+       collection =  get_website_collection();
+       websiteCount = collection.count_documents({ 'domain' : domain})
+       return websiteCount > 0
+
+def insert_website(websiteObj):
+       collection =  get_website_collection();
+       response  = collection.insert_one(websiteObj)
+       return response.inserted_id
+
+def update_website(query, websiteObj):
+       collection =  get_website_collection();
+       response  = collection.update_one(query, {"$set":websiteObj})
+       return response.upserted_id
+
+
+def search_websites(embedding, limit=10, skip=0):
+       collection =  get_website_collection();
+       websites = collection.aggregate([
+              {
+                     "$vectorSearch": {
+                            "index": ATLAS_VECTOR_SEARCH_INDEX_NAME,
+                            "queryVector": embedding,
+                            "numCandidates":100,
+                            "path": "summary",
+                            "limit": limit,
+                     }
+              },
+              {
+              "$project": {
+                     "summary_text": 1,
+                     "domain":1,
+                     "title": 1,
+                     "keywords": 1,
+                     "updated_at": 1,
+                     "summary": 1,
+                     "score": { "$meta": "vectorSearchScore" }
+              }
+              },
+              {
+                     "$match": {
+                     "score": { "$gt": 0.65 }
+                     }
+              }
+       ])
+       return websites
---
+++ your_app.py
@@ -0,0 +1,57 @@
+import os
+from sentence_transformers import SentenceTransformer, util
+import time
+import streamlit as st
+from db_store.mongo import search_websites
+
+def get_embeddings_huggingface(sentences):
+       model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+       embeddings = model.encode(sentences)
+       return embeddings
+
+
+# MongoDB connection details
+MONGO_URI = os.getenv('MONGO_URI')
+DB_NAME = os.getenv('DB_NAME')
+COLLECTION_NAME = os.getenv('COLLECTION_NAME')
+
+# Function to perform similarity search
+def perform_similarity_search(query):
+    # Assume that the collection has precomputed embeddings stored
+    print("Fetting Embeddings")
+    query_embedding = get_embeddings_huggingface([query])
+    
+    # Compute cosine similarity
+    print("Computing Similarity")
+    docs = search_websites(query_embedding[0].tolist())
+    return docs
+
+# Streamlit UI
+st.title('Website Similarity Search')
+
+# Input form for user query
+query = st.text_input('Enter your query')
+
+# Perform search if query is provided
+if query:
+    start_time = time.time()  # Start timer
+    
+    results = perform_similarity_search(query)
+    
+    end_time = time.time()  # End timer
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    
+    st.write(f"Showing results for: {query}")
+    st.write(f"Time taken: {elapsed_time:.2f} seconds")  # Display elapsed time
+    
+    for result in results:
+        st.markdown(f"### Title: {result['title']}")
+        st.markdown(f"**Domain:** {result['domain']}")
+        st.markdown(f"**Description:** {result['summary_text'][0:100]}")
+        st.markdown(f"**Score:** {result['score']}")
+        st.markdown(f"**Keywords:** {', '.join(result['keywords'])}")
+        st.markdown("---")
+
+# Clear button
+if st.button('Clear'):
+    st.experimental_rerun()
